87d014ec5fcb451cbbaf2dcd79ad85e61f693189
Remi <remi_bruneau@hotmail.fr>
2021-11-08 17:02:04 -0500

    App configuration complété??? - 0960718

--- WeatherApp/App.xaml.cs
+++ WeatherApp/App.xaml.cs
@@ -9,5 +9,9 @@ namespace WeatherApp
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            string key = AppConfiguration.GetValue("apiKey");
+        }
     }
 }
---
+++ WeatherApp/AppConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public static class AppConfiguration
+    {
+        private static IConfiguration configuration;
+
+        public static string GetValue(String conf)
+        {
+            if (configuration == null)
+                InitConfig();
+            return configuration.GetValue<string>(conf);
+        }
+
+        private static void InitConfig()
+        {
+            configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json", true, true).AddUserSecrets("e0a86c66-f550-437b-a2c9-3260f364ea05", true).Build();
+        }
+    }
+}
