cd91e63b90adb62bea1fc9f463a4f9709b1817e0
Remi <remi_bruneau@hotmail.fr>
2021-11-08 17:34:24 -0500

    OpenWetherService Complété - 0960718

---
+++ WeatherApp/Services/OpenWetherService.cs
@@ -0,0 +1,28 @@
+﻿using OpenWeatherAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Models;
+
+namespace WeatherApp.Services
+{
+    class OpenWetherService : IWindDataService
+    {
+        private OpenWeatherProcessor owp;
+        public WindDataModel LastWindData;
+
+        public OpenWetherService(string apiKey)
+        {
+            owp = OpenWeatherProcessor.Instance;
+            owp.ApiKey = apiKey;
+        }
+        public async Task<WindDataModel> GetDataAsync()
+        {
+            Task<OWCurrentWeaterModel> task = owp.GetCurrentWeatherAsync();
+            await task;
+            return LastWindData = new WindDataModel { DateTime = DateTime.FromOADate(task.Result.DateTime), Direction = task.Result.Wind.Deg, MetrePerSec = task.Result.Wind.Speed };
+        }
+    }
+}
